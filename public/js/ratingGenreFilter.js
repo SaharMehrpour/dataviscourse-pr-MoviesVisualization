3898ca7320f346ba2c8e2d7b07f393c1fd8683dd
ZahraFahim <zahra.fahimfar@utah.edu>
2016-11-29 17:27:47 -0700

    Changes for year filter and rating genre filter.

--- public/js/ratingGenreFilter.js
+++ public/js/ratingGenreFilter.js
@@ -1,4 +1,9 @@
+var MovieGenres = [
+    "Drama","Action","Adventure","Comedy","Crime","Family","Fantasy",
+    "Thriller","Sci-fi","Mystery","Romance","Horror","Other"];
 
+var genresvg;
+var leg;
 /*
  Constructor
  */
@@ -23,7 +28,6 @@ RatingGenreFilter.prototype.init = function(){
     // select from the chart
     // self.interactivity.updatedGenreRatingFilter(selectedGenreRating);
 
-    var self = this;
     self.margin = {top: 10, right: 20, bottom: 30, left: 50};
     var divyearChart = d3.select("#rating_genre_filter_div");
     self.svgBounds = divyearChart.node().getBoundingClientRect();
@@ -32,15 +36,11 @@ RatingGenreFilter.prototype.init = function(){
     self.svg = divyearChart.append("svg")
         .attr("width",self.svgWidth)
         .attr("height",self.svgHeight)
-        .style("background-color", "Tan");
+        .attr("class","svgratinggenre");
 
-//"Action","Adventure","Animation","Comedy","Crime","Family","Fantasy","Thriller","Sci-Fi","Drama","Mystery"
-    // ,"Romance","Biography","History"," War","Horror","Sport","Western","Documentary","Music/Musical","Short","Film-Noir"
-    //,"News","Reality-TV","Game-Show"
 
-    var MovieGenres = [
-        "Drama","Action","Adventure","Comedy","Crime","Family","Fantasy","Thriller","Sci-fi","Mystery","Romance","Horror","Other"
-    ];
+    genresvg=self.svg;
+
 
     var x =  d3.scaleBand().domain(MovieGenres).range([10,self.svgWidth-50]);
 
@@ -49,9 +49,7 @@ RatingGenreFilter.prototype.init = function(){
     self.svg.append("g")
         .attr("transform", "translate(30,480)")
         .call(xaxis)
-        .style("font-size","15px")
-        .style("font-weight", "bold")
-        .style("font-family", "Times New Roman")
+        .attr("class","axistext")
         .selectAll("text")
         .style("text-anchor", "end")
         .attr("dx", "-.8em")
@@ -66,19 +64,67 @@ RatingGenreFilter.prototype.init = function(){
     self.svg.append("g")
         .attr("transform", "translate(40,0)")
         .call(yaxis)
-        .style("font-size","17px")
-        .style("font-weight", "bold")
-        .style("font-family", "Times New Roman")
+        .attr("class","axistext")
+
     ;
 
-    d3.csv("data/movie_metadata.csv", function (error, movies)
+    self.svg.append("text")
+        .attr("transform",
+            "translate(" + (self.svgWidth/2) + " ," +
+            (self.svgHeight + self.margin.top - 30) + ")")
+        .attr("class","xlabel")
+        .text("Genre (Movies May Contain Multiple Genres)");
+
+    self.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 45 - self.margin.left)
+        .attr("x", 350-(self.svgHeight ))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("font-size","20px")
+        .style("font-weight", "bold")
+        .style("font-family", "Times New Roman")
+        .text("Rating");
+
+
+    self.svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(30,480)")
+        .call(xaxis
+            .tickSize(-(self.svgHeight-170))
+            .tickFormat("")
+        );
+
+    self.svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(40,0)")
+        //-43
+        .call(yaxis
+            .tickSize(-(self.svgWidth-80))
+            .tickFormat("")
+            .ticks(9)
+        );
+
+
+    leg=self.svg.append("g")
+        .attr("class", "legend")
+        .style("font-size",14)
+        .attr("transform","translate(70,15)")
+    ;
+  /*  d3.csv("data/movie_metadata.csv", function (error, movies)
     {
-        //console.log(movies);
+
         var data = d3.nest().key(function (d) {
             return Math.round(d["imdb_score"]);
         }).entries(movies);
 
-        //   console.log(data);
+        var filter=data.filter(function(d){
+                return d.key>8;
+
+        });
+
+        console.log("filter2",filter);
+
         var GenresList = new Array(13);
         for (i=0; i<=10; i++) GenresList[i]=new Array(25);
 
@@ -152,8 +198,8 @@ RatingGenreFilter.prototype.init = function(){
             return d.count;
         });
 
-        var range = ["#0066CC", "#0080FF", "#3399FF", "#66B2FF", "#99ccff", "#CCE5FF", "#ffcccc", "#ff9999", "#ff6666", "#ff3333", "#FF0000", "#CC0000"];
-        var color = d3.scaleQuantile().domain([min, max]).range(range);
+
+        var color = d3.scaleQuantile().domain([min, max]).range(colorbrewer.Reds[8]);
 
             //d3.scaleLinear().domain([min, max]).range(["yellow", "green"]);
 
@@ -197,12 +243,32 @@ RatingGenreFilter.prototype.init = function(){
 
 
 
+       var linearSize = d3.scaleLinear().domain([min, max]).range([7,20]);
+        var colorleg = d3.scaleLinear().domain([min, max]).range(colorbrewer.Reds[8]);
+
+
+        leg = self.svg.append("g")
+            .attr("class", "legendSize")
+            .attr("transform", "translate(850, 70)");
+
+        var legendSize = d3.legendSize()
+            .scale(linearSize)
+            .shape('circle')
+            .shapePadding(15)
+            .labelOffset(20)
+           // .orient('horizontal')
+        ;
+
+        leg.call(legendSize);
 
 
 
-    })
 
 
+
+    })*/
+
+self.update(self.movies);
 
 
 
@@ -211,8 +277,205 @@ RatingGenreFilter.prototype.init = function(){
 /**
  * update the chart according to the new movie set
  */
-RatingGenreFilter.prototype.update = function(newMovieSet) {
+RatingGenreFilter.prototype.update = function(newMovieset) {
+
     var self = this;
+
+    var data = d3.nest().key(function (d) {
+        return Math.round(d["imdb_score"]);
+    }).entries(newMovieset);
+
+    self.svg=genresvg;
+
+        var GenresList = new Array(13);
+        for (i=0; i<=10; i++) GenresList[i]=new Array(25);
+
+        for(i=0;i<=10;i++)
+            for(j=0;j<=12;j++)
+                GenresList[i][j]=0;
+
+        var temp = data.filter(function (d)
+        {
+            for( i=0;i<d.values.length;i++)
+            {
+
+                if(d.values[i]["genres"].indexOf("Drama")!=-1) GenresList[d.key][0]++;
+                if(d.values[i]["genres"].indexOf("Action")!=-1) GenresList[d.key][1]++;
+                if(d.values[i]["genres"].indexOf("Adventure")!=-1) GenresList[d.key][2]++;
+                if(d.values[i]["genres"].indexOf("Animation")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Comedy")!=-1) GenresList[d.key][3]++;
+                if(d.values[i]["genres"].indexOf("Crime")!=-1) GenresList[d.key][4]++;
+                if(d.values[i]["genres"].indexOf("Family")!=-1) GenresList[d.key][5]++;
+                if(d.values[i]["genres"].indexOf("Fantasy")!=-1) GenresList[d.key][6]++;
+                if(d.values[i]["genres"].indexOf("Thriller")!=-1) GenresList[d.key][7]++;
+                if(d.values[i]["genres"].indexOf("Sci")!=-1) GenresList[d.key][8]++;
+                if(d.values[i]["genres"].indexOf("Mystery")!=-1) GenresList[d.key][9]++;
+                if(d.values[i]["genres"].indexOf("Romance")!=-1) GenresList[d.key][10]++;
+                if(d.values[i]["genres"].indexOf("Biography")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("History")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("War")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Horror")!=-1) GenresList[d.key][11]++;
+                if(d.values[i]["genres"].indexOf("Sport")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Western")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Documentary")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Music")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Musical")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Short")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Film-Noir")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("News")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Reality-TV")!=-1) GenresList[d.key][12]++;
+                if(d.values[i]["genres"].indexOf("Game-Show")!=-1) GenresList[d.key][12]++;
+            }
+
+            return null;
+        });
+
+
+        var final = new Array();
+        var xscale = 51;
+        var yscale = 42;
+        var xindex=69;
+        var yindex=55;
+        for(i =0;i<=12;i++)
+        {
+            for(j =10;j>=2;j--)
+            {
+                var movie ={};
+                movie.name = MovieGenres[i] + " " +j;
+                movie.x = xindex;
+                movie.y = yindex;
+                yindex=yindex+yscale;
+                movie.count = GenresList[j][i];
+                final.push(movie);
+            }
+            xindex=xindex+xscale;
+            yindex=55;
+        }
+
+    var min = d3.min(final, function (d)
+        {
+            return d.count;
+        });
+
+    var max = d3.max(final, function (d)
+        {
+            return d.count;
+        });
+
+
+
+    var color = d3.scaleQuantile()
+                .domain([min, max])
+                .range(colorbrewer.PuRd[8])
+        ;
+
+    var size = d3.scaleLinear()
+            .domain([min, max])
+            .range([Math.log(min+5)*3.5, Math.log(max)*3.5]);
+
+
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
+    rating = self.svg.selectAll("circle").data(final);
+    rating.exit().remove();
+    rating = rating.enter().append("circle").classed("ratinggenre","true").merge(rating);
+
+
+    rating
+        .attr("r",function(d)
+            {
+                if(d.count>5)
+                return size(d.count);
+            })
+        .attr("cy",function(d)
+            {
+                return d.y;
+            })
+        .attr("cx",function(d)
+            {
+                return d.x;
+            })
+        .style("fill", function (d)
+            {
+                return color(d.count);
+            })
+        .style("stroke","black")
+        .on("mouseover", function(d)
+        {
+
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                 div.html("#movies" + "<br/>"  +d.count)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+
+                 d3.select(this).style("fill","red");
+
+        })
+        .on("mouseout", function(d)
+        {
+
+                div.transition()
+                .duration(500)
+                .style("opacity", 0);
+
+             d3.select(this).style("fill", function (d)
+             {
+                return color(d.count);
+             });
+
+        })
+    ;
+
+
+   // d3.selectAll(".legend").remove();
+
+    var legendQuantile = d3.legendColor()
+       .labelFormat(d3.format(",.0f"))
+       .shape('circle')
+       .shapePadding(60)
+       .orient('horizontal')
+       .labelOffset(7)
+       .scale(color)
+    ;
+    leg.call(legendQuantile)
+        //.selectAll("text")
+        //.html( + "<br/>"  +"color")
+       // .attr("transform", function(d) {return "rotate(-90)"})
+
+    ;
+
+
+
+
+
+    /*  var linearSize = d3.scaleLinear().domain([min, max]).range([7,20]);
+
+
+         leg = self.svg.append("g")
+             .attr("class", "legendSize")
+             .attr("transform", "translate(40, 25)");
+
+         var legendSize = d3.legendSize()
+                 .scale(size)
+                 .shape('circle')
+                 .shapePadding(15)
+                 .labelOffset(20)
+                 .orient('horizontal')
+             ;
+
+         leg.call(legendSize);
+
+ */
+
+
+
+
 
 };
 
--- public/js/yearFilter.js
+++ public/js/yearFilter.js
@@ -1,4 +1,4 @@
-
+var genredata;
 /*
 Constructor
  */
@@ -16,56 +16,161 @@ function YearFilter(movies, interactivity) {
 /**
  * Initializes the chart
  */
-YearFilter.prototype.init = function(){
-
-   // var self = this;
-
-   // var selectedYears;
+YearFilter.prototype.init = function() {
+    // var selectedYears;
     // select based on brush
-  //  self.interactivity.updatedYearFilter(selectedYears);
+    //  self.interactivity.updatedYearFilter(selectedYears);
 
     var self = this;
+
     self.margin = {top: 10, right: 20, bottom: 30, left: 50};
     var divyearChart = d3.select("#year_filter_div");
     self.svgBounds = divyearChart.node().getBoundingClientRect();
     self.svgWidth = self.svgBounds.width - self.margin.left - self.margin.right;
-    self.svgHeight = 100;
+    self.svgHeight = 200;
     self.svg = divyearChart.append("svg")
-        .attr("width",self.svgWidth)
-        .attr("height",self.svgHeight);
+        .attr("width", self.svgWidth)
+        .attr("height", self.svgHeight);
 
 
-    var x =  d3.scaleLinear().domain([1910,2020]).range([10,self.svgWidth-30]);
-    var xaxis=d3.axisBottom(x).tickFormat(d3.format("d"));
+    movies = self.movies
+    genredata = movies;
+    var data = d3.nest().key(function (d) {
+        return Math.round(d["imdb_score"]);
+    }).entries(movies);
+
+
+    //genredata=data;
+    //RatingGenreFilter.prototype.update(data);
+
+    movies.forEach(function (d) {
+        d.year = +d.title_year;
+    });
+
+    var data = d3.nest().key(function (d) {
+        return d.year;
+    }).sortKeys(d3.ascending).rollup(function (leaves) {
+        return leaves.length;
+    }).entries(movies);
+
+
+    var x = d3.scaleTime().domain([1916, 2016]).range([0, self.svgWidth - 30]);
+    var xaxis = d3.axisBottom(x).tickFormat(d3.format("d"));
+
+
+    var areaFn = d3.area()
+        .x(function (d) {
+            return x(d.key);
+        })
+        .y0(self.svgHeight / 2)
+        .y1(function (d) {
+            return -Math.log(d.value) * 10 + self.svgHeight / 2;
+        });
+
+    self.svg.append("path")
+        .attr("fill", "pink")
+        .attr("d", areaFn(data))
+    ;
+
+    var lineFn = d3.line()
+        .x(function (d) {
+            return x(d.key);
+        })
+        .y(function (d) {
+            return -Math.log(d.value) * 10 + self.svgHeight / 2;
+        });
+
+    var line = self.svg.append("path")
+            .attr("fill", "none")
+            .style("stroke", "darkmagenta")
+            .style("stroke-width", "3px")
+            .attr("d", lineFn(data))
+        ;
 
     self.svg.append("g")
-        .attr("transform", "translate(10,40)")
+        .attr("transform", "translate(" + (0) + " ," +
+            (self.svgHeight / 2) + ")")
         .call(xaxis)
-        .style("font-size","20px")
+        .style("font-size", "20px")
         .style("font-weight", "bold")
         .style("font-family", "Times New Roman")
-     ;
+    ;
+
+
+    var firstyear = 1916;
+    var lastyear = 2016;
+    startyear = self.svg.selectAll(".startyear").data([1]);
+    startyear.exit().remove();
+    startyear = startyear.enter().append("text").classed("startyear", "true").merge(startyear);
+    startyear.text(firstyear + " - " + lastyear).attr("x", self.svgWidth / 2 - 40).attr("y", self.svgHeight / 2 - 70);
+
 
     function brushed() {
+
+
         var extent = brush.extent();
-        var scale=-17;
-        var interval=82;
-        var start=d3.event.selection[0]+scale;
-        var end=d3.event.selection[1]+scale;
-        //console.log(d3.event.selection[0],d3.event.selection[1]);
-       console.log(1910+(Math.floor(start/interval)*10)+Math.floor((10*(start%interval))/interval),
-           1910+(Math.floor(end/interval)*10)+Math.floor((10*(end%interval))/interval));
+        var scale = -3;
+        var interval = 69;
+        var start = d3.event.selection[0] + scale;
+        var end = d3.event.selection[1] + scale;
+        firstyear = 1916 + (Math.floor(start / interval) * 10) + Math.floor((10 * (start % interval)) / interval);
+        lastyear = 1916 + (Math.floor(end / interval) * 10) + Math.floor((10 * (end % interval)) / interval);
+
+        //console.log(1910+(Math.floor(start/interval)*10)+Math.floor((10*(start%interval))/interval),
+        //1910+(Math.floor(end/interval)*10)+Math.floor((10*(end%interval))/interval));
+        //console.log(start,end);
+        startyear.text(firstyear + " - " + lastyear).attr("x", self.svgWidth / 2 - 40).attr("y", self.svgHeight / 2 - 70);
+
+/*
+        var filterdata = genredata.filter(function (d) {
+            return d.title_year > firstyear &&
+                d.title_year < lastyear;
+        });
+        //console.log(filterdata);
+        var updateddata = d3.nest().key(function (d) {
+            return Math.round(d["imdb_score"]);
+        }).entries(filterdata);
+   */
+        //(updateddata);
+
 
     };
 
-    var brush = d3.brushX().extent([[0,0],[self.svgWidth,self.svgHeight-50]])
-        .on("end", brushed);
+    var leftHandle = self.svg.append("image")
+        .attr("width", 25)
+        .attr("height", 90)
+        .attr("x", -100)
+        .attr("xlink:href", 'images/yearFilter/left-handle.png');
 
-    self.svg.append("g")
-        .attr("transform", "translate(0,20)")
-        .attr("class", "brush").call(brush);
+    var rightHandle = self.svg.append("image")
+        .attr("width", 25)
+        .attr("height", 90)
+        .attr("x", -100)
+        .attr("xlink:href", 'images/yearFilter/right-handle.png');
+
+    var brush = d3.brushX().extent([[8, self.svgHeight / 2 - 70], [self.svgWidth - 30, self.svgHeight / 2 + 20]])
+        .on("end", brushed)
+        .on("brush", function () {
+            leftHandle.attr("x", d3.event.selection[0] - 16);
+            leftHandle.attr("y", self.svgHeight / 2 - 60);
+            rightHandle.attr("x", d3.event.selection[1] - 10);
+            rightHandle.attr("y", self.svgHeight / 2 - 60);
 
 
+        });
+
+
+    var brushg = self.svg.append("g")
+            .attr("transform", "translate(0,10)")
+            .attr("class", "brush")
+            .call(brush)
+        ;
+
+    resetBrush = function () {
+        //console.log("za");
+        // brushg.call(brush.extent([[0,0],[0,0]]));
+        d3.select("brush").call(brush.extent([[0, 0], [0, 0]]));
+    }
 
 
 };
